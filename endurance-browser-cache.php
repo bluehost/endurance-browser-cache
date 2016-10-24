3d8f7eb45ea07b40b9a3ce98c17b4f8034fc5fff
Mike <web.mhansen@gmail.com>
2016-10-24 14:50:21 -0600

    Add initial code.

---
+++ endurance-browser-cache.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Plugin Name: Endurance Browser Cache
+Description: Browser caching.
+Version: 0.1
+Author: Mike Hansen
+Author URI: https://www.mikehansen.me/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Do not access file directly!
+if ( ! defined( 'WPINC' ) ) { die; }
+
+define( 'EBC_VERSION', 0.1 );
+
+
+if ( ! class_exists( 'Endurance_Browser_Cache' ) ) {
+	class Endurance_Browser_Cache {
+		function __construct() {
+			$this->hooks();
+		}
+
+		function hooks() {
+			add_filter( 'mod_rewrite_rules', array( $this, 'htaccess_contents' ) );
+		}
+
+		function htaccess_contents( $rules ) {
+			$default_files = array(
+				'image/jpg' => '1 year',
+				'image/jpeg' => '1 year',
+				'image/gif' => '1 year',
+				'image/png' => '1 year',
+				'text/css' => '1 month',
+				'application/pdf' => '1 month',
+				'text/javascript' => '1 month',
+			);
+
+			$file_types = wp_parse_args( get_option( 'ebc_filetype_expirations', array() ), $default_files );
+
+			$additions = "<IfModule mod_expires.c>\n\tExpiresActive On\n";
+			foreach ( $file_types as $file_type => $expires ) {
+				$additions .= 'ExpiresByType ' . $file_type . ' "access plus ' . $expires . '"' . "\n\t";
+			}
+
+			$additions .= "\tExpiresByType image/x-icon \"access plus 1 year\"\n\tExpiresDefault \"access plus 1 weeks\"\n</IfModule>";
+			return $additions . $rules;
+		}
+	}
+	$ebc = new Endurance_Browser_Cache;
+}
