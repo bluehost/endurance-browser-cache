9b7691c0afb519e0523b4e528850aec0c916d051
Mike <web.mhansen@gmail.com>
2016-11-09 14:34:49 -0700

    Add abiltiy to enable/disable the feature without disabling the plugin.

--- endurance-browser-cache.php
+++ endurance-browser-cache.php
@@ -14,6 +14,7 @@ if ( ! defined( 'WPINC' ) ) { die; }
 
 define( 'EBC_VERSION', 0.1 );
 
+
 if ( ! class_exists( 'Endurance_Browser_Cache' ) ) {
 	class Endurance_Browser_Cache {
 		function __construct() {
@@ -21,8 +22,10 @@ if ( ! class_exists( 'Endurance_Browser_Cache' ) ) {
 		}
 
 		function hooks() {
-			add_filter( 'mod_rewrite_rules', array( $this, 'htaccess_contents' ) );
-			add_action( 'update_option_ebc_filetype_expirations', 'save_mod_rewrite_rules' );
+			if ( $this->is_enabled() ) {
+				add_filter( 'mod_rewrite_rules', array( $this, 'htaccess_contents' ) );
+			}
+			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'status_link' ) );
 		}
 
 		function htaccess_contents( $rules ) {
@@ -45,6 +48,32 @@ if ( ! class_exists( 'Endurance_Browser_Cache' ) ) {
 
 			$additions .= "ExpiresByType image/x-icon \"access plus 1 year\"\n\tExpiresDefault \"access plus 1 weeks\"\n</IfModule>\n";
 			return $additions . $rules;
+		}
+
+		function is_enabled() {
+			$cache_settings = get_option( 'mm_cache_settings' );
+			if ( isset( $_GET['ebc_toggle'] ) ) {
+				$valid_values = array( 'enabled', 'disabled' );
+				if ( in_array( $_GET['ebc_toggle'], $valid_values ) ) {
+					$cache_settings['browser'] = $_GET['ebc_toggle'];
+					update_option( 'mm_cache_settings', $cache_settings );
+					header( 'Location: ' . admin_url( 'plugins.php?plugin_status=mustuse' ) );
+				}
+			}
+			if ( isset( $cache_settings['browser'] ) && 'disabled' == $cache_settings['browser'] ) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		function status_link( $links ) {
+			if ( $this->is_enabled() ) {
+				$links[] = '<a href="' . add_query_arg( array( 'ebc_toggle' => 'disabled' ) ) . '">Disable</a>';
+			} else {
+				$links[] = '<a href="' . add_query_arg( array( 'ebc_toggle' => 'enabled' ) ) . '">Enable</a>';
+			}
+			return $links;
 		}
 	}
 	$ebc = new Endurance_Browser_Cache;
