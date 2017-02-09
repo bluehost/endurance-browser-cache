f3edec288c51cf1e7636167c7e80f8dd41734223
Mike <web.mhansen@gmail.com>
2017-02-09 13:52:32 -0700

    Bump constant.

--- endurance-browser-cache.php
+++ endurance-browser-cache.php
@@ -12,7 +12,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 // Do not access file directly!
 if ( ! defined( 'WPINC' ) ) { die; }
 
-define( 'EBC_VERSION', 0.1 );
+define( 'EBC_VERSION', 0.2 );
 
 
 if ( ! class_exists( 'Endurance_Browser_Cache' ) ) {
