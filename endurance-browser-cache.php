3a10c2a8a3452623c0dc5a2af44d48bacfa73107
Mike <web.mhansen@gmail.com>
2017-04-09 17:09:56 -0600

    Remove code so that if a user has both plugins active there is no duplicate code.

--- endurance-browser-cache.php
+++ endurance-browser-cache.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Endurance Browser Cache
 Description: Browser caching.
-Version: 0.3
+Version: 0.4
 Author: Mike Hansen
 Author URI: https://www.mikehansen.me/
 License: GPLv2 or later
@@ -12,70 +12,10 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 // Do not access file directly!
 if ( ! defined( 'WPINC' ) ) { die; }
 
-define( 'EBC_VERSION', 0.3 );
+define( 'EBC_VERSION', 0.4 );
 
-
-if ( ! class_exists( 'Endurance_Browser_Cache' ) ) {
-	class Endurance_Browser_Cache {
-		function __construct() {
-			$this->hooks();
-		}
-
-		function hooks() {
-			if ( $this->is_enabled() ) {
-				add_filter( 'mod_rewrite_rules', array( $this, 'htaccess_contents' ), 88 );
-			}
-			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'status_link' ) );
-		}
-
-		function htaccess_contents( $rules ) {
-			$default_files = array(
-				'image/jpg'       => '1 year',
-				'image/jpeg'      => '1 year',
-				'image/gif'       => '1 year',
-				'image/png'       => '1 year',
-				'text/css'        => '1 month',
-				'application/pdf' => '1 month',
-				'text/javascript' => '1 month',
-				'text/html'       => '2 hours',
-			);
-
-			$file_types = wp_parse_args( get_option( 'ebc_filetype_expirations', array() ), $default_files );
-
-			$additions = "<IfModule mod_expires.c>\n\tExpiresActive On\n\t";
-			foreach ( $file_types as $file_type => $expires ) {
-				$additions .= 'ExpiresByType ' . $file_type . ' "access plus ' . $expires . '"' . "\n\t";
-			}
-
-			$additions .= "ExpiresByType image/x-icon \"access plus 1 year\"\n\tExpiresDefault \"access plus 1 weeks\"\n</IfModule>\n";
-			return $additions . $rules;
-		}
-
-		function is_enabled() {
-			$cache_settings = get_option( 'mm_cache_settings' );
-			if ( isset( $_GET['ebc_toggle'] ) && is_admin() ) {
-				$valid_values = array( 'enabled', 'disabled' );
-				if ( in_array( $_GET['ebc_toggle'], $valid_values ) ) {
-					$cache_settings['browser'] = $_GET['ebc_toggle'];
-					update_option( 'mm_cache_settings', $cache_settings );
-					header( 'Location: ' . admin_url( 'plugins.php?plugin_status=mustuse' ) );
-				}
-			}
-			if ( isset( $cache_settings['browser'] ) && 'disabled' == $cache_settings['browser'] ) {
-				return false;
-			} else {
-				return true;
-			}
-		}
-
-		function status_link( $links ) {
-			if ( $this->is_enabled() ) {
-				$links[] = '<a href="' . add_query_arg( array( 'ebc_toggle' => 'disabled' ) ) . '">Disable</a>';
-			} else {
-				$links[] = '<a href="' . add_query_arg( array( 'ebc_toggle' => 'enabled' ) ) . '">Enable</a>';
-			}
-			return $links;
-		}
-	}
-	$ebc = new Endurance_Browser_Cache;
-}
+/*
+Code for the Endurance Browser Cache Plugin integrated into the Endurance Page Cache plugin.
+See merge for more information:
+https://github.com/bluehost/endurance-page-cache/commit/fc66c1963adef251bd0741acb1827d5cbc19941b
+*/
